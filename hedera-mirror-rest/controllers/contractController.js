2f5a6be6e90cc106a08d95375506873b35709c3b
nikolay <n.atanasow94@gmail.com>
2022-05-30 18:56:03 +0300

    Expose the ethereum hash
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- hedera-mirror-rest/controllers/contractController.js
+++ hedera-mirror-rest/controllers/contractController.js
@@ -866,7 +866,12 @@ class ContractController extends BaseController {
 
     const rows = await ContractService.getContractResultsByIdAndFilters(conditions, params, order, limit);
     const response = {
-      results: rows.map((row) => new ContractResultViewModel(row)),
+      results: rows.map((row) => {
+        return {
+          ...new ContractResultViewModel(row),
+          hash: row.hash ? utils.toHexString(row.hash, true) : null,
+        };
+      }),
       links: {
         next: null,
       },
